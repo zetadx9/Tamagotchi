bfc64e5119c09eb0d0719d7538d4b9d516a563c8
zetadx9 <beatsrus44_44@yahoo.com>
2020-06-24 16:04:22 -0400

    Commit 3 Added Jquery

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('Hello World')
+// console.log('Hello World')
+
+
+class tamagotchi {
+    constructor(name, creatureType) {
+        this.name = name
+        this.creatureType = creatureType
+        this.foodInTummy = 10
+        this.restedness = 10
+        this.health = 10
+        this.stopIntervals = []
+      }
+}
