ea3d4b14d4a3c18936b56cbcc6d76d81d76c8d87
zetadx9 <beatsrus44_44@yahoo.com>
2020-06-25 09:44:22 -0400

    Commit 8 Last day to work

--- app.js
+++ app.js
@@ -3,56 +3,84 @@
 
 class tamagotchi {
     constructor(name) {
-        this.name = name
-        this.creatureType = 'Digimon'
-        this.hunger = 10
-        this.sleepiness = 10
-        this.boredom = 10
-        this.health = 10
-        this.age = 0
-        this.stopIntervals = []
-      }
+        this.name = name;
+        this.creatureType = 'Digimon';
+        this.hunger = 10;
+        this.sleepiness = 10;
+        this.boredom = 10;
+        this.age = 0;
+          this.tired = function() {
+            this.sleepiness--;
+            console.log('Bleeeeh! Sick!!!!!!');
+            console.log(this.name + ' has current health = ' + this.sleepiness);
+          };
+          this.bored = function(){
+            this.boredom--;
+            console.log('Yaaaawwwwn! So tired');
+            console.log(this.name + ' has current restedness = ' + this.boredom);
+          };
+          this.start = function(){
+            console.log("Starting " + this.name);
+            var self = this;
+            this.hungerTimer = setInterval(function() {
+              self.cry();
+            }, 6000);
+            this.yawnTimer = setInterval(function() {
+              self.tired();
+            }, 10000);
+            this.boredTimer = setInterval(function() {
+              self.bored();
+            }, 25000);
+          };
+          this.stop = function(){
+            console.log("Stopping " + this.name);
+            clearInterval(this.hungerTimer);
+            clearInterval(this.yawnTimer);
+            clearInterval(this.boredTimer);
+          };
+        };
 }
 
 
 // Start Game
 
-document.getElementById("play-game").addEventListener("click", gameStart);
+//document.getElementById("play-game").addEventListener("click", gameStart);
+$("#play-game").on("click",gameStart)
 
 function gameStart () {
-
+console.log("gameStart");
 }
 
 
 
 // Pet Feed
-document.getElementById("food").addEventListener("click", petHungry);
+//document.getElementById("food").addEventListener("click", petHungry);
+$("#food").on("click",petHungry)
 
-function petHungry () {
-    while(this.hunger <= 10) {
-    this.hunger -= 1;
-    if(this.hunger === 0)
-    {
-        console.log('You died');
-    }
-    break;
-}
-}
+ function petHungry () {
+    this.hunger--;
+    console.log("WAAAH!!!!!!");
+    console.log(this.name + ' has current food in tummy = ' + tamagotchi.this.hunger);
+ }
+ //petHungry();
 
 
 // Pet Rest
-document.getElementById("sleep").addEventListener("click", petSleepy);
+//document.getElementById("sleep").addEventListener("click", petSleepy);
+$("#sleep").on("click",petSleepy)
 
 function petSleepy () {
+console.log("petSleepy");
 
 }
 
 
 // Pet Play Time
-document.getElementById("play").addEventListener("click", petBored);
+//document.getElementById("play").addEventListener("click", petBored);
+$("#play").on("click",petBored)
 
 function petBored () {
-
+console.log("Play");
 }
 
 // Pet Health
