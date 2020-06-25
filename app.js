12a87866343dcf3c409d0cf57d27a79999dd2d07
zetadx9 <beatsrus44_44@yahoo.com>
2020-06-25 16:12:08 -0400

    Commit 15 got the food button to work

--- app.js
+++ app.js
@@ -19,13 +19,14 @@ class tamagotchi {
 
 //document.getElementById("play-game").addEventListener("click", gameStart);
 $("#play-game").on("click",gameStart)
+
 console.log($("#age").val());
+
+let count = 0;
 
 function gameStart () {
 console.log("Game has started!");
 
-let num = parseInt($('#hungerNum'));
-let count = 0;
 function myCount() {
     // your original if-else wasn't allowing count to increment past 1
     if (count > 10) {
@@ -34,29 +35,50 @@ function myCount() {
     $('#hungerNum').text(count);
     // increment count regardless of its current value
     count++;
+} setInterval(myCount, 1000); 
+
+
+$("#food").on("click",petHungry)
+
+function petHungry() {
+    // your original if-else wasn't allowing count to increment past 1
+    if (count < 10) {
+        count = count - 1 ;
+    }
+    $('#hungerNum').text(count);
+    // increment count regardless of its current value
 }
 
-setInterval(myCount, 500); 
+
+
+setInterval(function() {
+    $('#change').attr('src','image2.gif');
+  }, 3000, clearInterval()); // <- 1000ms = 1s
+
+  setInterval(function() {
+    $('#change').attr('src','image3.gif');
+  }, 6000, clearInterval()); // <- 1000ms = 1s
+
+  setInterval(function() {
+    $('#change').attr('src','image4.gif');
+  }, 9000, clearInterval()); // <-  1000ms = 1s
+
+  setInterval(function() {
+    $('#change').attr('src','image5.gif');
+  }, 12000, clearInterval()); // <- 1000ms = 1s
   
+
+ 
+
+
 }
-
-
-
-
-
-
-
 
 
 // Pet Feed
 //document.getElementById("food").addEventListener("click", petHungry);
-$("#food").on("click",petHungry)
 
- function petHungry () {
-    this.hunger--;
-    console.log("WAAAH!!!!!!");
-    console.log(this.name + ' has current food in tummy = ' + tamagotchi.this.hunger);
- }
+
+    
  //petHungry();
 
 
