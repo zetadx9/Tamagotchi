445f85f2c7908a128346003e35d6caf129d6aca6
zetadx9 <beatsrus44_44@yahoo.com>
2020-06-24 21:49:24 -0400

    Started functionality of Tamagotchi

--- app.js
+++ app.js
@@ -2,13 +2,63 @@
 
 
 class tamagotchi {
-    constructor(name, creatureType) {
+    constructor(name) {
         this.name = name
-        this.creatureType = creatureType
+        this.creatureType = 'Digimon'
         this.hunger = 10
         this.sleepiness = 10
         this.boredom = 10
-        this.age = age
+        this.health = 10
+        this.age = 0
         this.stopIntervals = []
       }
 }
+
+
+// Start Game
+
+document.getElementById("play-game").addEventListener("click", gameStart);
+
+function gameStart () {
+
+}
+
+
+
+// Pet Feed
+document.getElementById("food").addEventListener("click", petHungry);
+
+function petHungry () {
+    while(this.hunger <= 10) {
+    this.hunger -= 1;
+    if(this.hunger === 0)
+    {
+        console.log('You died');
+    }
+    break;
+}
+}
+
+
+// Pet Rest
+document.getElementById("sleep").addEventListener("click", petSleepy);
+
+function petSleepy () {
+
+}
+
+
+// Pet Play Time
+document.getElementById("play").addEventListener("click", petBored);
+
+function petBored () {
+
+}
+
+// Pet Health
+
+
+function petHealth () {
+
+}
+ 
