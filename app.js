fc8827dea034dd9972bad83349f00e94c819b6df
zetadx9 <beatsrus44_44@yahoo.com>
2020-06-25 20:16:58 -0400

    Final Commit Im tired lol

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ class tamagotchi {
 
 
 
-// Start Game
+// Start Game / Hatch Button
 
 //document.getElementById("play-game").addEventListener("click", gameStart);
 $("#play-game").on("click",gameStart)
@@ -27,35 +27,39 @@ let Bcount = 0;
 let aging = 0;
 
 function gameStart () {
-console.log("Game has started!");
+            console.log("Game has started!");
 
-// $("#food").on("click",petHungry)
-// function hactch () {
-//     console.log("Game has started!");
-
-let endTimer = setInterval(myCount, 1000); 
+//End Timer-----------------------------------
+            let endTimer = setInterval(myCount, 1000); 
 
 
+// Game Function
 
-
-function myCount() {
-    // your original if-else wasn't allowing count to increment past 1
+        function myCount() {
+            // your original if-else wasn't allowing count to increment past 1
     if (count >= 10 || Scount >= 10 || Bcount >=10 || aging >= 10) {
        clearInterval(endTimer);
        console.log("You Lose");
     }
-    if(count < 0)
-    {
-        count = 0;
-    }
-    if(Scount < 0)
-    {
-        Scount = 0;
-    }
-    if(Bcount < 0)
-    {
-        Bcount = 0;
-    }
+         if(count < 0)
+       {
+         count = 0;
+       }
+
+         if(Scount < 0)
+       {
+          Scount = 0;
+       }
+
+         if(Bcount < 0)
+       {
+          Bcount = 0;
+       }
+
+         if (aging < 0)
+       {
+          aging = 0;
+       }
 
     $('#hungerNum').text(count);
     $('#sleepNum').text(Scount);
@@ -67,81 +71,89 @@ function myCount() {
     Bcount++;
 } 
 
-$("#food").on("click",petHungry)
+// Food Button
+    $("#food").on("click",petHungry)
 
-function petHungry() {
+     function petHungry() {
     // your original if-else wasn't allowing count to increment past 1
-    if (count < 10) {
-        count = count - 1 ;
+         if (count < 10) {
+            count = count - 1 ;
+          }
+      $('#hungerNum').text(count);
+      // increment
     }
-    $('#hungerNum').text(count);
-    // increment count regardless of its current value
-}
 
-$("#sleep").on("click",petSleep)
+           // Sleep buuton
+    $("#sleep").on("click",petSleep)
 
-function petSleep() {
+       function petSleep() {
     // your original if-else wasn't allowing count to increment past 1
     if (Scount < 10) {
         Scount = Scount - 1 ;
-    }
-    $('#sleepNum').text(Scount);
+     }
+       $('#sleepNum').text(Scount);
     // increment count regardless of its current value
-}
+     }
 
-$("#play").on("click",petPlay)
+           // Play Button
+    $("#play").on("click",petPlay)
 
-function petPlay() {
-    // your original if-else wasn't allowing count to increment past 1
-    if (Bcount < 10) {
+         function petPlay() {
+
+            if (Bcount < 10) {
         Bcount = Bcount - 1 ;
-    }
-    $('#boredNum').text(Bcount);
-    // increment count regardless of its current value
-}
+      }
+      $('#boredNum').text(Bcount);
 
-function myAgeCount() {
+    }
+
+           // Age Function
+          function myAgeCount() {
     // your original if-else wasn't allowing count to increment past 1
-    if (aging > 10) {
-        aging = 0;
-    }
-    if (count >= 10 || Scount >= 10 || Bcount >=10 ) {
-        clearInterval(endTimer);
-        console.log("Timer Stopped");
-    }
-    if(aging === 4)
-    {
-  $('#change').attr('src','image2.gif');   
-    }
+         if (aging > 10) {
+             aging = 0;
+             }
+         if (count >= 10 || Scount >= 10 || Bcount >=10 ) {
+         clearInterval(endTimer);
+         console.log("Timer Stopped");
+       }
+         if(aging === 0)
+       {
+       $('#change').attr('src','image0.gif');   
+       }
+      if(aging === 2)
+     {
+     $('#change').attr('src','image1.gif');   
+      }
+      if(aging === 4)
+      {
+      $('#change').attr('src','image2.gif');   
+     }
     
-    if(aging === 6)
-    {
-  $('#change').attr('src','image3.gif');   
-    }
+      if(aging === 6)
+     {
+    $('#change').attr('src','image3.gif');   
+     }
     
-    if(aging === 8)
-    {
-  $('#change').attr('src','image4.gif');   
-    }
+      if(aging === 8)
+      {
+     $('#change').attr('src','image4.gif');   
+      }
     
     if(aging === 10)
-    {
-  $('#change').attr('src','image5.gif');   
-    }
+      {
+     $('#change').attr('src','image5.gif');   
+      }
 
-    if(aging === 10)
-    {
+     if(aging === 10)
+       {
         console.log('You Win');
-    }
-    $('#ageNum').text(aging);
+       }
+     $('#ageNum').text(aging);
     // increment count regardless of its current value
     aging++;
-} setInterval(myAgeCount, 5000); 
+       } setInterval(myAgeCount, 5000); 
 
-
-}
-
-
-
+       }
     
  
